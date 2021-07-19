066c65b810e2bcb673c575ad71a6c93ca2d474f8
AriannaSchelfi <80247490+AriannaSchelfi@users.noreply.github.com>
2021-07-19 16:01:06 +0200

    Update numeroDiAbitanti.py

--- Covid19UltimoGiorno/libreria/numeroDiAbitanti.py
+++ Covid19UltimoGiorno/libreria/numeroDiAbitanti.py
@@ -1,18 +1,21 @@
-#In questo file calcolo il numero di abitanti in Italia e in Veneto sulla base del file platea che si trova su github nella cartella dei vaccini. Da qui ricavo il numero di abitanti aventi un età dai 12 anni in su. Il numero di abitanti è quello che si ricava dall'ultimo aggiornamento del file platea
+#In questo file calcolo il moltiplicatore per ogni regione (ovvero il rapporto tra popolazione italiana e popolazione regionale)
 import pandas as pd
 
-vacciniPlatea= pd.read_csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv", sep=",")
+#Leggiamo il csv della Protezione Civile contenente la popolazione delle regioni
+databasePopolazione= pd.read_csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv", sep=",")
 
-numeroDiAbitantiItaliani=vacciniPlatea["totale_generale"].sum()
+numeroDiAbitantiItaliani=datebasePopolazione["totale_generale"].sum() #Totale popolazione italiana
 
-#Calcoliamo il moltiplicatore per ogni regione
+#Funzione che calcola il moltiplicatore della regione passata come input
 def moltiplicatoreRegione(regione):
+	#Nel database considerato le regioni non si chiamano "P.A. Trento" e "P.A. Bolzano", ma "Trento" e "Bolzano"
 	if regione=="P.A. Trento":
 		regione="Trento"
 	if regione=="P.A. Bolzano":
 		regione="Bolzano"
-	vacciniPlateaRegione=vacciniPlatea[vacciniPlatea["denominazione_regione"]==regione]
-	numeroDiAbitantiRegione=vacciniPlateaRegione["totale_generale"].sum()
+		
+	popolazioneRegione=databasePopolazione[databasePopolazione["denominazione_regione"]==regione]
+	numeroDiAbitantiRegione=popolazioneRegione["totale_generale"].sum()
 	moltiplicatore=numeroDiAbitantiItaliani/numeroDiAbitantiRegione
 	print("Gli italiani sono "+str(moltiplicatore)+" volte gli abitanti di "+str(regione))
 	
