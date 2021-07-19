f84e5337a6b8a784842a2b9a870f36dd51bcca44
AriannaSchelfi <80247490+AriannaSchelfi@users.noreply.github.com>
2021-07-19 17:30:21 +0200

    regionePerRegione sistemata

--- Covid19DaInizioPandemia/libreria/graficiCovidRegionePerRegione.py
+++ Covid19DaInizioPandemia/libreria/graficiCovidRegionePerRegione.py
@@ -6,6 +6,116 @@ from libreria.numeroDiAbitanti import *
 
 pd.options.mode.chained_assignment = None
 
+def tamponiTotaliSettimanali(regione,covid19,listaDomeniche):
+
+	#Positivi su tutti i tamponi
+	listaSettimanaleTassoDiPositivitaInRegione=[]
+
+	for i in range(listaDomeniche.index("17-01-2021"), len(listaDomeniche)-1):
+
+		tamponiSettimanali = covid19[covid19['data']==listaDomeniche[i+1]]['tamponi'] - covid19[covid19['data']==listaDomeniche[i]]['tamponi']
+		positiviSettimanali = covid19[covid19['data']==listaDomeniche[i+1]]['totale_casi'] - covid19[covid19['data']==listaDomeniche[i]]['totale_casi']
+
+		rapportoSettimanale = positiviSettimanali*100/tamponiSettimanali
+		rapportoSettimanaleInRegione= rapportoSettimanale[regione]
+
+		listaSettimanaleTassoDiPositivitaInRegione.append(rapportoSettimanaleInRegione)
+	return listaSettimanaleTassoDiPositivitaInRegione
+
+
+def tamponiMolecolariSettimanaliRegione(regione,covid19,listaDomeniche):
+	
+	#Tasso di positivita ai tamponi molecolari
+	listaSettimanaleTassoDiPositivitaMolecolareInRegione=[]
+	
+
+	tamponiMolecolariSettimanali = covid19[covid19['data']==listaDomeniche[0]]['tamponi'] 
+	positiviMolecolariSettimanali= covid19[covid19['data']==listaDomeniche[0]]['totale_casi'] 
+
+	rapportoMolecolareSettimanale = positiviMolecolariSettimanali*100/tamponiMolecolariSettimanali
+	rapportoMolecolareSettimanaleInRegione= rapportoMolecolareSettimanale[regione]
+
+	listaSettimanaleTassoDiPositivitaMolecolareInRegione.append(rapportoMolecolareSettimanaleInRegione)
+
+	for i in range(listaDomeniche.index("17-01-2021")-1):
+		tamponiMolecolariSettimanali = covid19[covid19['data']==listaDomeniche[i+1]]['tamponi'] - covid19[covid19['data']==listaDomeniche[i]]['tamponi']
+
+		positiviMolecolariSettimanali= covid19[covid19['data']==listaDomeniche[i+1]]['totale_casi'] - covid19[covid19['data']==listaDomeniche[i]]['totale_casi']
+		    
+		rapportoMolecolareSettimanale = positiviMolecolariSettimanali*100/tamponiMolecolariSettimanali
+		rapportoMolecolareSettimanaleInRegione= rapportoMolecolareSettimanale[regione]
+		listaSettimanaleTassoDiPositivitaMolecolareInRegione.append(rapportoMolecolareSettimanaleInRegione)
+
+	#settimanaMista
+	tamponiMolecolariSettimanali = covid19[covid19['data']=='17-01-2021']['tamponi_test_molecolare'] - covid19[covid19['data']=='10-01-2021']['tamponi']
+	positiviMolecolariSettimanali = covid19[covid19['data']=='17-01-2021']['totale_positivi_test_molecolare'] - covid19[covid19['data']=='10-01-2021']['totale_casi']
+
+	rapportoMolecolareSettimanale= positiviMolecolariSettimanali*100/tamponiMolecolariSettimanali
+
+	rapportoMolecolareSettimanaleInRegione= rapportoMolecolareSettimanale[regione]
+	listaSettimanaleTassoDiPositivitaMolecolareInRegione.append(rapportoMolecolareSettimanaleInRegione)
+
+	#Settimane dal 15 in poi
+	for i in range(listaDomeniche.index("17-01-2021"),len(listaDomeniche)-1):
+		tamponiMolecolariSettimanali = covid19[covid19['data']==listaDomeniche[i+1]]['tamponi_test_molecolare'] - covid19[covid19['data']==listaDomeniche[i]]['tamponi_test_molecolare']
+
+		positiviMolecolariSettimanali=covid19[covid19['data']==listaDomeniche[i+1]]['totale_positivi_test_molecolare'] - covid19[covid19['data']==listaDomeniche[i]]['totale_positivi_test_molecolare']
+
+		rapportoMolecolareSettimanale = positiviMolecolariSettimanali*100/tamponiMolecolariSettimanali
+		rapportoMolecolareSettimanaleInRegione= rapportoMolecolareSettimanale[regione]
+		listaSettimanaleTassoDiPositivitaMolecolareInRegione.append( rapportoMolecolareSettimanaleInRegione)
+	return listaSettimanaleTassoDiPositivitaMolecolareInRegione
+
+
+#Terapie intensive
+def terapieIntensiveRegione(regione,covid19,listaDomeniche,moltiplicatore):
+	listaSettimanaleTerapiaIntensivaInRegione=[]
+
+	terapiaIntensivaSettimanale=covid19[covid19["data"]==listaDomeniche[0]]['terapia_intensiva']
+	terapiaIntensivaSettimanaleInRegione=terapiaIntensivaSettimanale[regione]
+	terapiaIntensivaSettimanaleInItalia=terapiaIntensivaSettimanale.sum()
+	listaSettimanaleTerapiaIntensivaInRegione.append(terapiaIntensivaSettimanaleInRegione*moltiplicatore)
+	for i in range(len(listaDomeniche)-1):
+		terapiaIntensivaSettimanale=covid19[covid19["data"]==listaDomeniche[i+1]]['terapia_intensiva'] - covid19[covid19["data"]==listaDomeniche[i]]['terapia_intensiva']
+		terapiaIntensivaSettimanaleInRegione=terapiaIntensivaSettimanale[regione]
+		listaSettimanaleTerapiaIntensivaInRegione.append(terapiaIntensivaSettimanaleInRegione*moltiplicatore)
+	return listaSettimanaleTerapiaIntensivaInRegione
+
+
+	#Terapie non intensive
+
+def ricoveriRepartiOrdinariRegione(regione,covid19,listaDomeniche,moltiplicatore):
+	listaSettimanaleOspedaliNonTerapiaIntensivaInRegione=[]
+
+	totaleOspedalizzati=covid19[covid19["data"]==listaDomeniche[0]]['totale_ospedalizzati']
+	terapiaIntensivaSettimanale=covid19[covid19["data"]==listaDomeniche[0]]['terapia_intensiva']
+	ospedalizzatiNonIntensiva=totaleOspedalizzati-terapiaIntensivaSettimanale
+	ospedalizzatiNonIntensivaInRegione=ospedalizzatiNonIntensiva[regione]
+	listaSettimanaleOspedaliNonTerapiaIntensivaInRegione.append(ospedalizzatiNonIntensivaInRegione*moltiplicatore)
+
+	for i in range(len(listaDomeniche)-1):
+		totaleOspedalizzati=covid19[covid19["data"]==listaDomeniche[i+1]]['totale_ospedalizzati']-covid19[covid19["data"]==listaDomeniche[i]]['totale_ospedalizzati']
+		terapiaIntensivaSettimanale=covid19[covid19["data"]==listaDomeniche[i+1]]['terapia_intensiva']-covid19[covid19["data"]==listaDomeniche[i]]['terapia_intensiva']
+
+		ospedalizzatiNonIntensiva=totaleOspedalizzati-terapiaIntensivaSettimanale
+
+		ospedalizzatiNonIntensivaInRegione=ospedalizzatiNonIntensiva[regione]
+		listaSettimanaleOspedaliNonTerapiaIntensivaInRegione.append(ospedalizzatiNonIntensivaInRegione*moltiplicatore)
+	return listaSettimanaleOspedaliNonTerapiaIntensivaInRegione
+
+
+def decedutiRegione(regione,covid19,listaDomeniche,moltiplicatore):
+	listaSettimanaleDecedutiInRegione=[]
+
+	decedutiSettimanale=covid19[covid19["data"]==listaDomeniche[0]]['deceduti']
+	decedutiSettimanaleInRegione=decedutiSettimanale[regione]
+	listaSettimanaleDecedutiInRegione.append(decedutiSettimanaleInRegione*moltiplicatore)
+	for i in range(len(listaDomeniche)-1):
+		decedutiSettimanale=covid19[covid19["data"]==listaDomeniche[i+1]]['deceduti']-covid19[covid19["data"]==listaDomeniche[i]]['deceduti']
+		decedutiSettimanaleInRegione=decedutiSettimanale[regione]
+		listaSettimanaleDecedutiInRegione.append(decedutiSettimanaleInRegione*moltiplicatore)
+	return listaSettimanaleDecedutiInRegione
+
 #ARGOMENTI: -regione di cui si vuole calcolare la letalità
 #	    -dataFrame covid19
 #	    -dataFrame con i dati del solo primo giorno di pandemia
