291ed7d90a2769e830df6cf424653ae619ed1e9a
AriannaSchelfi <80247490+AriannaSchelfi@users.noreply.github.com>
2021-07-19 15:01:23 +0200

    Update application.py

--- Covid19DaInizioPandemia/application.py
+++ Covid19DaInizioPandemia/application.py
@@ -1,17 +1,18 @@
+#File che chiama le tre funzioni principali: mappa (in file mappaCovid.py), graficiGiornalieri (in file graficiCovid.py) e graficiSettimanali (in file graficiCovidSettimanali.py)
 from libreria.graficiCovidSettimanali import *
 from libreria.graficiCovid import *
 from libreria.numeroDiAbitanti import *
-
 from libreria.mappaCovid import *
 from flask import Flask, jsonify, render_template
 
-
 application = Flask(__name__)
 
+#Per vedere se è tutto ok quando facciamo carica e distribuisci su AWS (sulla pagina html deve comparire la scritta "405 Method Not Allowed")
 @application.route("/")
 def index():
     return "405 Method Not Allowed"
 
+#funzione chiamata dal cron-job (aggiungendo "/update" all'url dell'applicazione caricata su AWS) 
 @application.route("/update")
 def update():
     mappa()
