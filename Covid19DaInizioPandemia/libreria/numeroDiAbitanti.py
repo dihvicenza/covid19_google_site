54889c61306ef81673ca9d46a7f1ce42842a8fa4
ChristopherSpennato <50098513+ChristopherSpennato@users.noreply.github.com>
2021-07-19 14:18:46 +0200

    A te

--- Covid19DaInizioPandemia/libreria/numeroDiAbitanti.py
+++ Covid19DaInizioPandemia/libreria/numeroDiAbitanti.py
@@ -1,26 +1,25 @@
-#In questo file calcolo il numero di abitanti in Italia e in ciascuna regione Veneto sulla base del file platea che si trova su github nella cartella dei vaccini. Da qui ricavo il numero di abitanti aventi un età dai 12 anni in su. Il numero di abitanti è quello che si ricava dall'ultimo aggiornamento del file platea
+#In questo file calcolo il numero di abitanti in Italia e in ciascuna regione in modo tale da calcolare il rapporto tra gli abitanti italiani e quelli di ciascuna regione
 
 import pandas as pd
-vacciniPlatea= pd.read_csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv", sep=",")
 
-#print (vacciniPlatea.info())
-numeroDiAbitantiItaliani=vacciniPlatea["totale_generale"].sum()
-vacciniPlateaVeneta=vacciniPlatea[vacciniPlatea["denominazione_regione"]=="Veneto"]
-numeroDiAbitantiVeneti=vacciniPlateaVeneta["totale_generale"].sum()
-moltiplicatore=numeroDiAbitantiItaliani/numeroDiAbitantiVeneti
+#Leggiamo il file contenente gli abitanti di ciascuna regione
+databasePopolazione= pd.read_csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv", sep=",")
 
-#Calcoliamo il moltiplicatore per ogni regione
+numeroDiAbitantiItaliani=databasePopolazione["totale_generale"].sum() #totale abitanti italiani
+popolazioneVeneta=databasePopolazione[databasePopolazione["denominazione_regione"]=="Veneto"] #database contenente solo i dati veneti
+numeroDiAbitantiVeneti=popolazioneVeneta["totale_generale"].sum() #Totale abitanti veneti
+moltiplicatore=numeroDiAbitantiItaliani/numeroDiAbitantiVeneti #rapporto italia/veneto
+
+#Funzione per calcolare il moltiplicatore della regione passata come argomento
 def moltiplicatoreRegione(regione):
+	#Nel database considerato le regioni non si chiamano "P.A. Trento" e "P.A. Bolzano", ma "Trento" e "Bolzano"
 	if regione=="P.A. Trento":
 		regione="Trento"
 	if regione=="P.A. Bolzano":
 		regione="Bolzano"
-	vacciniPlateaRegione=vacciniPlatea[vacciniPlatea["denominazione_regione"]==regione]
-	numeroDiAbitantiRegione=vacciniPlateaRegione["totale_generale"].sum()
-	moltiplicatore=numeroDiAbitantiItaliani/numeroDiAbitantiRegione
+	popolazioneRegione=databasePopolazione[databasePopolazione["denominazione_regione"]==regione] #Dati della regione 
+	numeroDiAbitantiRegione=popolazioneRegione["totale_generale"].sum() #Popolazione italiana
+	moltiplicatore=numeroDiAbitantiItaliani/numeroDiAbitantiRegione #Rapporto
 	print("Gli italiani sono "+str(moltiplicatore)+" volte gli abitanti di "+str(regione))
 	return moltiplicatore
 
-#print("Gli Italiani sono " +str(numeroDiAbitantiItaliani))
-#print(" I veneti sono "+str(numeroDiAbitantiVeneti))
-#print("Gli Italiani sono "+str(moltiplicatore) +" volte i Veneti")
